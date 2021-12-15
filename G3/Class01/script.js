cb802a062ef34cd4bcc0f0d03db672a920879942
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-12-15 19:57:46 +0100

    Class01 part-02

--- G3/Class01/script.js
+++ G3/Class01/script.js
@@ -1,5 +1,52 @@
 console.log("Hello from javascript!");
-alert("Hello from javascript!");
 
 
 console.log("Our second console log");
+
+let number;
+number = 5;
+
+console.log(number);
+
+let pi = 3.141592653589793;
+
+let a = "Trajan Stevkovski";
+let trainerName = "Trajan Stevkovski";
+let b = 33;
+let trainerAge = 33;
+let abc = "";
+let trainerFirstNameAndLastName = "Trajan Stevkovski";
+
+let isValid = true;
+
+let justDeclared;
+console.log("justDeclared");
+console.log(justDeclared);
+console.log(typeof justDeclared);
+
+let nullDefined = null;
+console.log("nullDefined");
+console.log(nullDefined);
+console.log(typeof nullDefined);
+
+let sumOfTwoNumbers = "3 + 7 = 10";
+console.log(sumOfTwoNumbers);
+console.log(typeof sumOfTwoNumbers);
+
+let num1 = 1.123;
+console.log(num1);
+console.log(typeof num1);
+
+// this variable is the name of the trainer
+let asd = "Trajan Stevkovski";
+
+// --- sungle line comment ---
+// validating this input and for the password we use pattern 1 capital letter 1 small letter 1 special sign and 1 number and it should be 8 chars long
+// complicated validating code...
+
+
+/*
+------multi line comment------
+let commentedVariable = 10;
+We can write here whatever we want, this is just a comment
+*/
