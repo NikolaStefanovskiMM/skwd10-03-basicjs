e3a8bf4a880f014cb236604f4c01c77ff24bf140
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-12-15 21:06:58 +0100

    Class01 part-03

--- G3/Class01/script.js
+++ G3/Class01/script.js
@@ -20,12 +20,15 @@ let trainerFirstNameAndLastName = "Trajan Stevkovski";
 let isValid = true;
 
 let justDeclared;
-console.log("justDeclared");
+console.log("--justDeclared--");
 console.log(justDeclared);
 console.log(typeof justDeclared);
 
+let somethingElse = "somethingElse";
+console.log(somethingElse);
+
 let nullDefined = null;
-console.log("nullDefined");
+console.log("--nullDefined--");
 console.log(nullDefined);
 console.log(typeof nullDefined);
 
@@ -50,3 +53,77 @@ let asd = "Trajan Stevkovski";
 let commentedVariable = 10;
 We can write here whatever we want, this is just a comment
 */
+
+
+console.log("console.log()");
+let numberFour = 4;
+console.log(numberFour); // this is using variable
+console.log(4); // this is using explicit value
+
+console.log("this is the value of variable named numberFour", numberFour);
+
+let ifThisIsTrue = false;
+
+console.log("======================== ARITHMETIC OPERATORS ====================================");
+
+let sum = 3 + 2; // = 5
+console.log(sum);
+
+let substract = sum - 2; // 5 - 2 = 3
+console.log(substract);
+
+let multiply = substract * sum; // 3 * 5 = 15 
+console.log(multiply);
+
+let division = 10 / 2; // = 5
+console.log(division);
+
+let modulus = 9 % 2; // = 1
+console.log(modulus);
+
+let incerment = 3;
+incerment++; // = 4
+console.log(incerment);
+
+let decrement = 3;
+decrement--; // = 2
+console.log(decrement);
+
+console.log("--------------- COMPARISON OPERATORS ------------------");
+
+let isLarger = 5 > 5; // false
+console.log(isLarger);
+
+let isLargerOrEven = 5 >= 5; // true
+console.log(isLargerOrEven);
+
+let isSmaler = 3 < 3; // fasle
+console.log(isSmaler);
+
+let isSmallerOrEven = 3 <= 3; // true
+console.log(isSmallerOrEven);
+
+let isEqual = 3 == 3; // true
+console.log(isEqual);
+
+let isNotEqual = 3 != 3; // fasle
+console.log(isNotEqual);
+
+let number5 = 3;
+let number6 = 5;
+let isLargerWithVariables = number5 < number6; // 3 < 5 = true
+
+console.log("--------------- ASSIGNEMENT OPERATORS ----------------------");
+
+let num2 = 2;
+let num3 = 3;
+
+num2 = num2 + num3; // 2 + 3 = 5
+console.log(num2);
+num2 += num3; // 5 + 3 = 8
+console.log(num2);
+
+num2 = num2 - num3; // 8 - 3 = 5
+console.log(num2);
+num2 -= num3 // 5 - 3 = 2
+console.log(num2); 
