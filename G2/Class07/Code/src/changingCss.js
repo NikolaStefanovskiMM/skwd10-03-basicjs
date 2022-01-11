a405a1e3e23e8563c579c56fd854908bc04fd650
tstojanovska <stojanovskatanja03@gmail.com>
2022-01-11 21:03:52 +0100

    Class 07

---
+++ G2/Class07/Code/src/changingCss.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("myButton");
+
+btn.addEventListener("click", function(){
+    let myDiv = document.getElementById("myDiv");
+    myDiv.style.color = "red";
+    myDiv.style.backgroundColor = "yellow";
+})
