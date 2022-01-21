17a52df2d923b0693b43dd75e19512a0cba3b130
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2022-01-21 20:08:34 +0100

    Class1 part-04

--- G3/Class11/AJAX/script.js
+++ G3/Class11/AJAX/script.js
@@ -38,3 +38,23 @@ $("#btn").on('click', function () {
 //         console.log(error);
 //     }
 // });
+
+function showData(response, from) {
+    console.log(from, response);
+} 
+
+$("#btn1").on('click', function () {
+    
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/users',
+        //method: "GET", // by default is get
+        success: showData,
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(showData)
+        .catch(error => console.log(error));
+});
